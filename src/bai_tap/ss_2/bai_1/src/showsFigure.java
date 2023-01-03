729366a71a9b24b038df95550098c19d8a2ddaab
c1122g1-lanh <lanh7108@gmail.com>
2023-01-03 11:09:47 +0700

    up

--- src/bai_tap/ss_2/bai_1/src/showsFigure.java
+++
@@ -1,5 +0,0 @@
-public class showsFigure {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ src/bai_tap/ss_2/showsFigure.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class showsFigure {
+    static String sao = "*";
+
+    public static void retangular() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(sao);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void triangular() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(sao + " ");
+            }
+            System.out.println(" ");
+        }
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(sao + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        retangular();
+        triangular();
+
+    }
+}
